d4ac059b0a58f7137c64ced6222cde1687efd0a0
yassobaja <yasso.baja@gmail.com>
2021-04-04 12:37:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var ytplayer_autoplay = false;
 var ytplayer_ready = false;
 var ytplayer_api_ready = false;
 
-var gapiKey = 'AIzaSyCw4rG9w1P8z_2qSQ6q_bSnsuhSs5wEjq4';
+var gapiKey = 'AIzaSyBqWvvDDHw7JNG-dm-mGuoBBetp2UGOH4Y';
+#AIzaSyCw4rG9w1P8z_2qSQ6q_bSnsuhSs5wEjq4
 function gapiLoad(){
 	gapi.client.setApiKey(gapiKey);
 	gapi.client.load('youtube', 'v3', function(response) {var scope = angular.element($('#videoctrl')).scope();scope.$apply(function(){scope.yt_api_ready = true;});});
